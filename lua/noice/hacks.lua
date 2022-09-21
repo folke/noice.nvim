8b03e87f1b90c8d58b1c13a70963664eb4cb8019
Folke Lemaitre <folke.lemaitre@gmail.com>
2022-09-21 21:28:27 +0200

    feat: hacks :)

---
+++ lua/noice/hacks.lua
@@ -0,0 +1,18 @@
+local M = {}
+
+local data = {}
+
+---@see https://github.com/neovim/neovim/issues/17810
+function M.fix_incsearch(enable)
+	if enable then
+		if data.conceallevel == nil then
+			data.conceallevel = vim.wo.conceallevel
+			vim.wo.conceallevel = 0
+		end
+	elseif data.conceallevel ~= nil then
+		vim.wo.conceallevel = data.conceallevel
+		data.conceallevel = nil
+	end
+end
+
+return M
