e359706b19b1c33a02b9eac41669e62c43f8ae97
Folke Lemaitre <folke.lemaitre@gmail.com>
2022-09-21 21:30:23 +0200

    perf: use uv timers instead of defer_fn

--- lua/noice/ui/init.lua
+++ lua/noice/ui/init.lua
@@ -3,12 +3,24 @@ local Util = require("noice.util")
 
 local M = {}
 
+local inside_redraw = false
+function M.redraw()
+	if inside_redraw then
+		return
+	end
+	inside_redraw = true
+	vim.cmd.redraw()
+	inside_redraw = false
+end
+
 M.attached = false
 
 function M.attach()
 	M.attached = true
-	vim.ui_attach(Config.ns, { ext_messages = true }, function(event, ...)
-		M.handle(event, ...)
+	vim.ui_attach(Config.ns, { ext_messages = true, ext_popupmenu = true }, function(event, ...)
+		if not inside_redraw then
+			M.handle(event, ...)
+		end
 	end)
 end
 
@@ -22,21 +34,23 @@ end
 function M.setup()
 	local group = vim.api.nvim_create_augroup("messages_ui", {})
 
-	vim.api.nvim_create_autocmd("CmdlineEnter", {
-		group = group,
-		callback = function()
-			M.detach()
-			-- vim.opt.cmdheight = 1
-			vim.cmd([[redraw]])
-		end,
-	})
+	if not Config.options.cmdline.enabled then
+		vim.api.nvim_create_autocmd("CmdlineEnter", {
+			group = group,
+			callback = function()
+				M.detach()
+				-- vim.opt.cmdheight = 1
+				vim.cmd([[redraw]])
+			end,
+		})
 
-	vim.api.nvim_create_autocmd("CmdlineLeave", {
-		group = group,
-		callback = function()
-			M.attach()
-		end,
-	})
+		vim.api.nvim_create_autocmd("CmdlineLeave", {
+			group = group,
+			callback = function()
+				M.attach()
+			end,
+		})
+	end
 
 	M.attach()
 end
