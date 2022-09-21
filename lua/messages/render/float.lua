c8b0c9215eb98aa349361aa7d93e3f8631f15e01
Folke Lemaitre <folke.lemaitre@gmail.com>
2022-09-21 08:04:32 +0200

    feat: use NuiPopup for floats

--- lua/messages/render/float.lua
+++ lua/messages/render/float.lua
@@ -1,42 +1,64 @@
-local function setup()
-	local buf = vim.api.nvim_create_buf(false, true)
-	local win = vim.api.nvim_open_win(buf, false, {
-		relative = "editor",
-		focusable = true,
-		width = 80,
-		height = 20,
-		anchor = "NE",
-		row = 1,
-		col = vim.o.columns,
-		style = "minimal",
-		border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
-	})
-	vim.api.nvim_buf_set_option(buf, "buftype", "nofile")
-	vim.api.nvim_buf_set_option(buf, "bufhidden", "wipe")
+local function setup(opts)
+	local Popup = require("nui.popup")
+	local event = require("nui.utils.autocmd").event
 
-	local function close()
-		if vim.api.nvim_buf_is_valid(buf) then
-			vim.api.nvim_buf_delete(buf, { force = true })
-		end
-		if vim.api.nvim_win_is_valid(win) then
-			vim.api.nvim_win_close(win, true)
-		end
+	opts = vim.tbl_deep_extend("force", {}, {
+		enter = true,
+		focusable = true,
+		border = {
+			style = "rounded",
+		},
+		position = "50%",
+		size = {
+			width = "80%",
+			height = "60%",
+		},
+		win_options = {
+			winhighlight = "Normal:NotifyINFOBody,FloatBorder:NotifyINFOBorder",
+		},
+	}, opts or {})
+
+	local popup = Popup(opts)
+	popup.border:set_highlight("NotifyINFOTitle")
+
+	-- mount/open the component
+	popup:mount()
+
+	-- unmount component when cursor leaves buffer
+	popup:on(event.BufLeave, function()
+		popup:unmount()
+	end, { once = true })
+
+	popup:on({ event.BufWinLeave }, function()
+		vim.schedule(function()
+			popup:unmount()
+		end)
+	end, { once = true })
+
+	popup:map("n", { "q", "<esc>" }, function()
+		popup:unmount()
+	end, { remap = false, nowait = true })
+
+	return popup
+end
+
+---@param renderer Renderer
+local function get_popup(renderer)
+	---@type NuiPopup
+	local popup = renderer.popup
+	if popup and popup.bufnr and vim.api.nvim_buf_is_valid(popup.bufnr) then
+		return popup
 	end
 
-	vim.keymap.set("n", "<ESC>", close, { buffer = buf, nowait = true })
-	vim.keymap.set("n", "q", close, { buffer = buf, nowait = true })
-	vim.api.nvim_create_autocmd({ "BufDelete", "BufLeave", "BufHidden" }, {
-		once = true,
-		buffer = buf,
-		callback = close,
+	renderer.popup = setup({
+		border = {
+			text = { top = " Messages " },
+		},
 	})
-	return buf, win
+	return renderer.popup
 end
 
 ---@param renderer Renderer
 return function(renderer)
-	if not (renderer.buf and vim.api.nvim_buf_is_valid(renderer.buf)) then
-		renderer.buf = setup()
-	end
-	renderer:render_buf(renderer.buf)
+	renderer:render_buf(get_popup(renderer).bufnr)
 end
