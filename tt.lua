6302ea9952d710856eab8b0fa3a9b8d83b2a901b
Folke Lemaitre <folke.lemaitre@gmail.com>
2022-09-22 10:37:14 +0200

    style: removed tt.lua

--- tt.lua
+++
@@ -1,9 +0,0 @@
-local NuiLine = require("nui.line")
-
-local line = NuiLine()
-
-line:append("Something\n Went\n Wrosssng!", "Error")
-
-local bufnr, ns_id, linenr_start = 0, -1, 1
-
-line:render(bufnr, ns_id, linenr_start)
