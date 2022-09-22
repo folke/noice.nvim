0b8b72fd4c2c515fd12bf6e82a20d93bd00b311a
Folke Lemaitre <folke.lemaitre@gmail.com>
2022-09-22 13:46:36 +0200

    style: attr_id is always an integer now

--- lua/noice/highlight.lua
+++ lua/noice/highlight.lua
@@ -32,9 +32,6 @@ function M.get_hl_group(attr_id)
 end
 
 function M.get_hl(attr_id)
-  if type(attr_id) == "string" then
-    return attr_id
-  end
   if not M.cache[attr_id] then
     local attrs = M.attr2entry(attr_id)
     local hl_group = M.get_hl_group(attr_id)
