b2c419cc9bf6a0240179ecca8311faebe5042922
Folke Lemaitre <folke.lemaitre@gmail.com>
2022-09-25 17:58:35 +0200

    feat: keep track of message update tick and added id to debug output

--- lua/noice/message.lua
+++ lua/noice/message.lua
@@ -2,15 +2,15 @@ local Block = require("noice.block")
 local Filter = require("noice.filter")
 local Config = require("noice.config")
 
-local id = 0
+local _id = 0
 
 ---@class NoiceMessage: NoiceBlock
 ---@field id number
 ---@field event NoiceEvent
 ---@field ctime number
 ---@field mtime number
+---@field tick number
 ---@field kind? NoiceKind
----@field expired boolean
 ---@diagnostic disable-next-line: undefined-field
 local Message = Block:extend("NoiceBlock")
 
@@ -18,19 +18,19 @@ local Message = Block:extend("NoiceBlock")
 ---@param kind? NoiceKind
 ---@param content? NoiceContent|NoiceContent[]
 function Message:init(event, kind, content)
-  id = id + 1
-  self.id = id
+  _id = _id + 1
+  self.id = _id
+  self.tick = 1
   self.ctime = vim.fn.localtime()
   self.mtime = vim.fn.localtime()
   self.event = event
   self.kind = kind
-  self.expired = false
   ---@diagnostic disable-next-line: undefined-field
   Message.super.init(self)
 
   if Config.options.debug then
     local NuiText = require("nui.text")
-    self:append(NuiText(event .. "." .. (kind or ""), "DiagnosticVirtualTextInfo"))
+    self:append(NuiText("[" .. self.id .. "] " .. event .. "." .. (kind or ""), "DiagnosticVirtualTextInfo"))
     self:append(NuiText(" ", "Normal"))
   end
 
