bb8b9b091ce08166f16ba490bb74fa263c6184e8
Folke Lemaitre <folke.lemaitre@gmail.com>
2022-09-26 13:51:20 +0200

    feat: added Noice command to show history

---
+++ lua/noice/commands.lua
@@ -0,0 +1,24 @@
+local View = require("noice.view")
+local Manager = require("noice.manager")
+local Config = require("noice.config")
+
+local M = {}
+
+---@type NoiceView?
+M._history_view = nil
+
+function M.setup()
+  vim.api.nvim_create_user_command("Noice", function()
+    if not M._history_view then
+      M._history_view = View.get_view(Config.options.history.view, Config.options.history.opts)
+    end
+    M._history_view:display(Manager.get(Config.options.history.filter, {
+      history = true,
+      sort = true,
+    }))
+  end, {
+    desc = "Open Noice Message History",
+  })
+end
+
+return M
--- lua/noice/init.lua
+++ lua/noice/init.lua
@@ -1,8 +1,13 @@
 local M = {}
 
 function M.setup()
+  if vim.fn.has("nvim-0.8.0") ~= 1 then
+    require("noice.util").error("Noice needs Neovim >= 0.8.0")
+    return
+  end
   require("noice.config").setup()
   require("noice.hacks").setup()
+  require("noice.commands").setup()
   require("noice.router").setup()
   require("noice.ui").setup()
 end
