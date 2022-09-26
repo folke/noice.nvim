04c6a8ed7e5aabe9bfe1a3e15ca6bb2db464f124
Folke Lemaitre <folke.lemaitre@gmail.com>
2022-09-26 16:55:38 +0200

    fix: fixed position and size of cmp popupmenu

--- lua/noice/ui/popupmenu.lua
+++ lua/noice/ui/popupmenu.lua
@@ -1,11 +1,24 @@
 local Config = require("noice.config")
+local Cmdline = require("noice.ui.cmdline")
 
 local M = {}
 
 ---@type cmp.CustomEntriesView
 M.view = nil
 
+M.no_cmdline_mode = false
+
+function M.fix_cmp()
+  local orig = require("cmp.utils.api").is_cmdline_mode
+  require("cmp.utils.api").is_cmdline_mode = function()
+    return orig() and not M.no_cmdline_mode
+  end
+  M.fix_cmp = function() end
+end
+
+---@param items string[][]
 function M.on_show(_, items, selected)
+  M.fix_cmp()
   if not M.view then
     M.view = require(
       "cmp.view." .. (Config.options.cmdline.menu == "wild" and "wildmenu" or "custom") .. "_entries_view"
@@ -26,20 +39,29 @@ function M.on_show(_, items, selected)
   local entries = {}
 
   for i, item in ipairs(items) do
-    local word, kind, menu, info = unpack(item)
+    local word, _, menu, info = unpack(item) --[[@as string ]]
     table.insert(
       entries,
       Entry.new(ctx, source, {
         label = word,
-        kind = kind,
         menu = menu,
         info = info,
         preselect = i == selected + 1,
       })
     )
   end
-
-  M.view:open(0, entries)
+  local cursor = Cmdline.message.cursor
+  if cursor then
+    local win = vim.fn.bufwinid(cursor.buf)
+    if win ~= -1 then
+      M.no_cmdline_mode = true
+      vim.api.nvim_win_call(win, function()
+        M.view:open(cursor.col + 1, entries)
+        vim.cmd([[redraw]])
+      end)
+      M.no_cmdline_mode = false
+    end
+  end
 end
 
 function M.on_select(_, selected)
