84f43f2c53178db997721067186b692f72aac33f
Folke Lemaitre <folke.lemaitre@gmail.com>
2022-09-28 21:54:11 +0200

    feat: check for notify during startup

--- lua/noice/init.lua
+++ lua/noice/init.lua
@@ -5,6 +5,10 @@ function M.setup()
     require("noice.util").error("Noice needs Neovim >= 0.8.0")
     return
   end
+  if not pcall(require, "notify") then
+    require("noice.util").error("Noice needs nvim-notify to work properly")
+    return
+  end
   require("noice.config").setup()
   require("noice.hacks").setup()
   require("noice.commands").setup()
