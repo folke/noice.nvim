083bc09494d61edbf169a2b84a3be50356991b68
Folke Lemaitre <folke.lemaitre@gmail.com>
2022-09-29 12:11:58 +0200

    feat: added sub commands to Noice

--- lua/noice/commands.lua
+++ lua/noice/commands.lua
@@ -8,17 +8,41 @@ local M = {}
 M._history_view = nil
 
 function M.setup()
-  vim.api.nvim_create_user_command("Noice", function()
-    if not M._history_view then
-      M._history_view = View.get_view(Config.options.history.view, Config.options.history.opts)
+  local commands = {
+    stats = function()
+      require("noice.manager").add(require("noice.stats").message())
+    end,
+    history = function()
+      if not M._history_view then
+        M._history_view = View.get_view(Config.options.history.view, Config.options.history.opts)
+      end
+      M._history_view:display(Manager.get(Config.options.history.filter, {
+        history = true,
+        sort = true,
+      }))
+      M._history_view:show()
+    end,
+  }
+
+  vim.api.nvim_create_user_command("Noice", function(args)
+    local cmd = vim.trim(args.args or "")
+    if commands[cmd] then
+      commands[cmd]()
+    else
+      commands.history()
     end
-    M._history_view:display(Manager.get(Config.options.history.filter, {
-      history = true,
-      sort = true,
-    }))
-    M._history_view:show()
   end, {
-    desc = "Open Noice Message History",
+    nargs = "?",
+    desc = "Noice",
+    complete = function(f, line, ...)
+      if line:match("^Noice %w+ ") then
+        return {}
+      end
+      local prefix = line:match("^Noice (%w*)")
+      return vim.tbl_filter(function(key)
+        return key:find(prefix) == 1
+      end, vim.tbl_keys(commands))
+    end,
   })
 end
 
