29db63f4db86e9426f70896f927330dd6cad2150
Folke Lemaitre <folke.lemaitre@gmail.com>
2022-10-02 16:19:35 +0200

    feat: updated requirements to 0.9.0 (nightly)

--- lua/noice/init.lua
+++ lua/noice/init.lua
@@ -1,8 +1,8 @@
 local M = {}
 
 function M.setup()
-  if vim.fn.has("nvim-0.8.0") ~= 1 then
-    require("noice.util").error("Noice needs Neovim >= 0.8.0")
+  if vim.fn.has("nvim-0.9.0") ~= 1 then
+    require("noice.util").error("Noice needs Neovim >= 0.9.0 (nightly)")
     return
   end
   if not pcall(require, "notify") then
