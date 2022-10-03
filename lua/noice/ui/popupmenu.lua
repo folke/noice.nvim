24dc1c4ab41e0633bb3075db9dfa7f843eddc434
Folke Lemaitre <folke.lemaitre@gmail.com>
2022-10-03 07:44:49 +0200

    feat: added proper cmp source noice_popupmenu that is used if no cmdline source was configured

--- lua/noice/ui/popupmenu.lua
+++ lua/noice/ui/popupmenu.lua
@@ -1,91 +1,96 @@
 local require = require("noice.util.lazy")
 
 local Util = require("noice.util")
-local Config = require("noice.config")
-local Cmdline = require("noice.ui.cmdline")
-
--- local foo = require("noice.highlight")
+local cmp = require("cmp")
+local cmp_config = require("cmp.config")
 
 local M = {}
 
----@type cmp.CustomEntriesView
-M.view = nil
+---@class Popupmenu
+---@field selected number
+---@field col number
+---@field row number
+---@field grid number
+---@field items string[][]
+M.state = {
+  visible = false,
+  items = {},
+}
 
-M.no_cmdline_mode = false
-
-function M.fix_cmp()
-  local orig = require("cmp.utils.api").is_cmdline_mode
-  require("cmp.utils.api").is_cmdline_mode = function()
-    return orig() and not M.no_cmdline_mode
-  end
-  M.fix_cmp = function() end
+---@class NoiceCmpSource: cmp.Source
+---@field before_line string
+---@field items {label: string}[]
+local source = {}
+source.new = function()
+  return setmetatable({
+    items = {},
+  }, { __index = source })
 end
 
----@param items string[][]
-function M.on_show(_, items, selected)
-  M.fix_cmp()
-  if not M.view then
-    M.view = require(
-      "cmp.view." .. (Config.options.cmdline.menu == "wild" and "wildmenu" or "custom") .. "_entries_view"
-    ).new()
+function source:get_keyword_pattern()
+  return [=[[^[:blank:]]*]=]
+end
+
+function source:complete(_params, callback)
+  if not M.state.visible then
+    return callback()
   end
 
-  require("cmp.config").set_onetime({
-    preselect = require("cmp.types").cmp.PreselectMode.Item,
-    completion = {
-      completeopt = "noselect",
-    },
-  })
+  local items = {}
 
-  local source = require("cmp.source").new("noice", {})
-  local ctx = require("cmp.context").new()
+  for i, item in ipairs(M.state.items) do
+    local word, _, _menu, _info = unpack(item) --[[@as string ]]
+    table.insert(items, {
+      label = word,
+      kind = cmp.lsp.CompletionItemKind.Variable,
+      preselect = i == (M.state.selected + 1),
+    })
+  end
 
-  local Entry = require("cmp.entry")
-  local entries = {}
+  callback({ items = items, isIncomplete = true })
+end
 
-  for i, item in ipairs(items) do
-    local word, _, menu, info = unpack(item) --[[@as string ]]
-    table.insert(
-      entries,
-      Entry.new(ctx, source, {
-        label = word,
-        menu = menu,
-        info = info,
-        preselect = i == selected + 1,
+M.setup = function()
+  vim.notify("Registering noice with cmp")
+  cmp.register_source("noice_popupmenu", source.new())
+  for _, mode in ipairs({ ":" }) do
+    if not cmp_config.cmdline[mode] then
+      cmp.setup.cmdline(mode, {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = "noice_popupmenu" },
+        }),
       })
-    )
-  end
-  local cursor = Cmdline.message.cursor
-  if cursor and cursor.buf_line then
-    local win = vim.fn.bufwinid(cursor.buf)
-    if win ~= -1 then
-      M.no_cmdline_mode = true
-      vim.api.nvim_win_set_cursor(win, { cursor.buf_line, cursor.col })
-      vim.api.nvim_win_call(win, function()
-        M.view:open(2, entries)
-        Util.redraw()
-      end)
-      M.no_cmdline_mode = false
+      cmp.core:prepare()
     end
+  end
+end
+M.setup = Util.once(M.setup)
+
+---@param items string[][]
+function M.on_show(_, items, selected, row, col, grid)
+  M.setup()
+  M.state = {
+    items = items,
+    visible = true,
+    selected = selected,
+    row = row,
+    col = col,
+    grid = grid,
+  }
+  if not cmp.core.view:visible() then
+    cmp.complete()
   end
 end
 
 function M.on_select(_, selected)
-  if M.view then
-    M.view:_select(selected + 1, { behavior = require("cmp.types").cmp.SelectBehavior.Select })
-  end
+  M.state.selected = selected
+  M.state.visible = true
+  cmp.complete()
 end
 
 function M.on_hide()
-  if M.view then
-    M.view:close()
-  end
-end
-
-function M.update()
-  if not M.popup then
-    return
-  end
+  M.state.visible = false
 end
 
 return M
