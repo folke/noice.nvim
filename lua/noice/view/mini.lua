312ac20daeae1ba73c300671bbf8d405419a33ef
Folke Lemaitre <folke.lemaitre@gmail.com>
2022-10-13 23:43:45 +0200

    feat(mini): use timers per message id for hiding instead of defer_fn

--- lua/noice/view/mini.lua
+++ lua/noice/view/mini.lua
@@ -1,7 +1,6 @@
 local require = require("noice.util.lazy")
 
 local View = require("noice.view")
-local Filter = require("noice.message.filter")
 local NuiView = require("noice.view.nui")
 
 ---@class NoiceMiniOptions
@@ -9,15 +8,17 @@ local NuiView = require("noice.view.nui")
 local defaults = { timeout = 5000 }
 
 ---@class MiniView: NoiceView
----@field active NoiceMessage[]
+---@field active table<number, NoiceMessage>
 ---@field super NoiceView
 ---@field view? NuiView
+---@field timers table<number, uv.Timer>
 ---@diagnostic disable-next-line: undefined-field
 local MiniView = View:extend("MiniView")
 
 function MiniView:init(opts)
   MiniView.super.init(self, opts)
   self.active = {}
+  self.timers = {}
   local view_opts = vim.deepcopy(self._opts)
   view_opts.type = "popup"
   view_opts.format = { "{message}" }
@@ -29,17 +30,40 @@ function MiniView:update_options()
   self._opts = vim.tbl_deep_extend("force", defaults, self._opts)
 end
 
+function MiniView:autohide(id)
+  if not self.timers[id] then
+    self.timers[id] = vim.loop.new_timer()
+  end
+  self.timers[id]:start(self._opts.timeout, 0, function()
+    self.active[id] = nil
+    self.timers[id] = nil
+    vim.schedule(function()
+      self:update()
+    end)
+  end)
+end
+
 function MiniView:show()
   for _, message in ipairs(self._messages) do
-    if #Filter.filter(self.active, { message = message }) == 0 then
-      table.insert(self.active, 1, message)
-      vim.defer_fn(function()
-        self.active = Filter.filter(self.active, { ["not"] = { message = message } })
-        self.view:display(self.active, { dirty = true })
-      end, self._opts.timeout)
-    end
+    self.active[message.id] = message
+    self:autohide(message.id)
   end
-  self.view:display(self.active, { dirty = true })
+  self:update()
 end
+
+function MiniView:update()
+  local active = vim.tbl_values(self.active)
+  table.sort(
+    active,
+    ---@param a NoiceMessage
+    ---@param b NoiceMessage
+    function(a, b)
+      return a.id < b.id
+    end
+  )
+  self.view:display(active, { dirty = true })
+end
+
+function MiniView:hide() end
 
 return MiniView
