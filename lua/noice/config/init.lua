a5e521e72bd98af23f037008b4c782fea01c6dba
Folke Lemaitre <folke.lemaitre@gmail.com>
2022-10-20 22:58:34 +0200

    feat!: lsp_progress is now enabled by default. Disable with config.lsp_progress.enabled=false

--- lua/noice/config/init.lua
+++ lua/noice/config/init.lua
@@ -58,7 +58,7 @@ M.defaults = {
     view = "notify",
   },
   lsp_progress = {
-    enabled = false,
+    enabled = true,
     -- Lsp Progress is formatted using the builtins for lsp_progress. See config.format.builtin
     -- See the section on formatting for more details on how to customize.
     --- @type NoiceFormat|string
