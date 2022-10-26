3da3f6d5be613ff7eb590c3e7f31c4e0cddf75d8
Ranjith Hegde <ranjithshegde@gmail.com>
2022-10-26 13:55:21 +0200

    fix(smart_move): Dont move `cmdline` view (#123)
    \#117 Added the smart_move function that avoids overlap. But it
    introduced some bugs with cmdline view set to `cmdline`. To avoid
    overlap with `mini` or other views, the cmdline itself would move to odd
    locations. This fixes that issue.

--- lua/noice/view/nui.lua
+++ lua/noice/view/nui.lua
@@ -47,7 +47,10 @@ end
 
 -- Check if other floating windows are overlapping and move out of the way
 function NuiView:smart_move()
-  if not (self._opts.type == "popup" and self._opts.relative and self._opts.relative.type == "editor") then
+  if
+    not (self._opts.type == "popup" and self._opts.relative and self._opts.relative.type == "editor")
+    or self._opts.view == "cmdline"
+  then
     return
   end
 
