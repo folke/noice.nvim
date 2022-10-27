f6cc07af4d9329c48fed9e22044f2a0ed8ac7d31
Folke Lemaitre <folke.lemaitre@gmail.com>
2022-10-27 14:48:23 +0200

    perf(lazy): set package.loaded when real module was loaded

--- lua/noice/util/lazy.lua
+++ lua/noice/util/lazy.lua
@@ -1,16 +1,25 @@
 return function(module)
+  local mod = nil
+
+  local function load()
+    if not mod then
+      mod = require(module)
+      package.loaded[module] = mod
+    end
+    return mod
+  end
   -- if already loaded, return the module
   -- otherwise return a lazy module
   return type(package.loaded[module]) == "table" and package.loaded[module]
     or setmetatable({}, {
       __index = function(_, key)
-        return require(module)[key]
+        return load()[key]
       end,
       __newindex = function(_, key, value)
-        require(module)[key] = value
+        load()[key] = value
       end,
       __call = function(_, ...)
-        return require(module)(...)
+        return load()(...)
       end,
     })
 end
