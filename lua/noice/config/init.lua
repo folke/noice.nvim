91c79a0c51ebb070163668c1156d5cc20c02af3a
Folke Lemaitre <folke.lemaitre@gmail.com>
2022-10-27 10:45:28 +0200

    feat: preset for inc_rename

--- lua/noice/config/init.lua
+++ lua/noice/config/init.lua
@@ -145,9 +145,11 @@ M.defaults = {
   ---@type NoicePresets
   presets = {
     -- you can enable a preset by setting it to true, or a table that will override the preset config
+    -- you can also add custom presets that you can enable/disable with enabled=true
     bottom_search = false, -- use a classic bottom cmdline for search
     command_palette = false, -- position the cmdline and popupmenu together
     long_message_to_split = false, -- long messages will be sent to a split
+    inc_rename = false, -- enables an input dialog for inc-rename.nvim
   },
   throttle = 1000 / 30, -- how frequently does Noice need to check for ui updates? This has no effect when in blocking mode.
   ---@type NoiceConfigViews
--- lua/noice/config/preset.lua
+++ lua/noice/config/preset.lua
@@ -95,6 +95,22 @@ M.presets = {
       },
     },
   },
+  inc_rename = {
+    cmdline = {
+      format = {
+        IncRename = {
+          pattern = "^:%s*IncRename%s+",
+          icon = " ",
+          conceal = true,
+          opts = {
+            relative = "cursor",
+            size = { min_width = 20 },
+            position = { row = -3, col = 0 },
+          },
+        },
+      },
+    },
+  },
 }
 
 return M
