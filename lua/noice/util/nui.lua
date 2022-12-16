d85a4d01774b5649dbcda8526a26f201dff5ade4
Folke Lemaitre <folke.lemaitre@gmail.com>
2022-12-16 12:30:42 +0100

    fix(nui): remove border text when style is `nil`, `"none"`, or `"shadow"`

--- lua/noice/util/nui.lua
+++ lua/noice/util/nui.lua
@@ -91,7 +91,7 @@ function M.normalize_popup_options(opts)
   end
 
   -- fix border text
-  if opts.border and opts.border.style == "none" then
+  if opts.border and (not opts.border.style or opts.border.style == "none" or opts.border.style == "shadow") then
     opts.border.text = nil
   end
   return opts
