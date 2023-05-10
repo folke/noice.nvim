c2a745a26ae562f1faecbf6177ac53377d2658d5
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-05-10 14:37:12 +0200

    feat(cmp): incude item.detail when it's not part of item.documentation

--- lua/noice/lsp/override.lua
+++ lua/noice/lsp/override.lua
@@ -13,10 +13,19 @@ function M.setup()
     Hacks.on_module("cmp.entry", function(mod)
       mod.get_documentation = function(self)
         local item = self:get_completion_item()
-        if item.documentation then
-          return Format.format_markdown(item.documentation)
+
+        local lines = item.documentation and Format.format_markdown(item.documentation) or {}
+        local ret = table.concat(lines, "\n")
+
+        if item.detail and not ret:find(item.detail, 1, true) then
+          local ft = self.context.filetype
+          local dot_index = string.find(ft, "%.")
+          if dot_index ~= nil then
+            ft = string.sub(ft, 0, dot_index - 1)
+          end
+          ret = ("```%s\n%s\n```\n%s"):format(ft, vim.trim(item.detail), ret)
         end
-        return {}
+        return vim.split(ret, "\n")
       end
     end)
   end
