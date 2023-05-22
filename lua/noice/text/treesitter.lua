7e2692b0c461da182a54ff2af4a35aea2bf8ea5c
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-05-22 14:36:29 +0200

    fix(treesitter): ignore weird invalid end_col errors. Fixes #473

--- lua/noice/text/treesitter.lua
+++ lua/noice/text/treesitter.lua
@@ -88,7 +88,7 @@ function M.highlight(buf, ns, range, lang)
       local is_spell = name == "spell"
 
       if hl and not is_spell then
-        vim.api.nvim_buf_set_extmark(buf, ns, start_row, start_col, {
+        pcall(vim.api.nvim_buf_set_extmark, buf, ns, start_row, start_col, {
           end_line = end_row,
           end_col = end_col,
           hl_group = hl,
