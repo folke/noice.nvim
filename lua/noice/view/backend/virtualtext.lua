e5a4c7a6ac3ef4c32f97a50c0b9b21e21c445c04
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-10-12 20:25:35 +0200

    fix(virtual): remove extmark from correct buffer where it was set. Fixes #464

--- lua/noice/view/backend/virtualtext.lua
+++ lua/noice/view/backend/virtualtext.lua
@@ -5,18 +5,20 @@ local View = require("noice.view")
 
 ---@class VirtualText: NoiceView
 ---@field extmark? number
+---@field buf? number
 ---@diagnostic disable-next-line: undefined-field
 local VirtualText = View:extend("VirtualTextView")
 
 function VirtualText:show()
   self:hide()
+  self.buf = vim.api.nvim_get_current_buf()
 
   ---@type number, number
   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
   line = line - 1
 
   if self._messages[1] then
-    self.extmark = vim.api.nvim_buf_set_extmark(0, Config.ns, line, col, {
+    self.extmark = vim.api.nvim_buf_set_extmark(self.buf, Config.ns, line, col, {
       virt_text_pos = "eol",
       virt_text = { { vim.trim(self._messages[1]:content()), self._opts.hl_group or "DiagnosticVirtualTextInfo" } },
       hl_mode = "combine",
@@ -26,7 +28,7 @@ end
 
 function VirtualText:hide()
   if self.extmark then
-    vim.api.nvim_buf_del_extmark(0, Config.ns, self.extmark)
+    vim.api.nvim_buf_del_extmark(self.buf, Config.ns, self.extmark)
   end
 end
 
