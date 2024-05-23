f119045f38792ad5311e5f9be7a879e4c1a95fe0
Emil Nymann Sølyst <98811735+emilnymann@users.noreply.github.com>
2024-05-24 00:16:58 +0200

    fix(markdown): ignore empty completion documentation tables (#820)
    * fix(markdown): ignore empty completion documentation tables
    
    * style(markdown): revert whitespace changes

--- lua/noice/lsp/format.lua
+++ lua/noice/lsp/format.lua
@@ -29,9 +29,12 @@ function M.format_markdown(contents)
       table.insert(parts, ("```\n%s\n```"):format(content.value))
     elseif Util.islist(content) then
       vim.list_extend(parts, M.format_markdown(content))
+    elseif type(content) == "table" and next(content) == nil then
+      goto continue
     else
       error("Unknown markup " .. vim.inspect(content))
     end
+    ::continue::
   end
 
   return vim.split(table.concat(parts, "\n"), "\n")
