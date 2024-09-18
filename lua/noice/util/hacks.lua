8c6a02438f869e79e4d343683bc198ebf4b10d49
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-09-18 20:02:53 +0200

    fix: dont redraw when exiting. Fixes #936. Fixes #921

--- lua/noice/util/hacks.lua
+++ lua/noice/util/hacks.lua
@@ -33,6 +33,11 @@ function M.fix_redraw()
     0,
     30,
     vim.schedule_wrap(function()
+      if Util.is_exiting() then
+        timer:stop()
+        timer:close()
+        return
+      end
       if not Util.is_search() then
         if vim.api.nvim__redraw then
           vim.api.nvim__redraw({ flush = true, cursor = true })
